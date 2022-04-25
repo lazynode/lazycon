fe05ec1508dd29c37cbeac09153cc82e97989b8b
vang1ong7ang <vang1ong7ang@outlook.com>
2022-04-25 13:53:44 +0000

    Update index.js (#4)
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,20 @@
 window.addEventListener('NEOLine.N3.EVENT.READY', () => {
-    let neolineN3 = new NEOLineN3.Init();
-    document.getElementById("exec").addEventListener("keyup", function (event) {
-        if (event.code !== "Enter") return;
-        const url = new URL(event.target.value);
-        if (url.protocol !== "n3:") return;
-        const repathname = new RegExp('^//(.*):(.*)@([a-zA-Z0-9]+)/([a-zA-Z0-9]+)$')
-        const [_, sender, signer, scripthash, method] = url.pathname.match(repathname);
-        const args = [...url.searchParams].map(([k, v]) => ({ type: k, value: v }))
-        var invoke = {
-            scriptHash: scripthash,
-            operation: method,
-            args: args,
-            signers: [
-                {
-                    account: signer,
-                    scopes: 1
-                }
-            ],
+    const neolineN3 = new NEOLineN3.Init();
+    document.getElementById("exec").addEventListener("keyup", async function (event) {
+        try {
+            if (event.code !== "Enter") return;
+            const url = new URL(event.target.value);
+            if (url.protocol !== "n3:") throw 'protocol must be n3!';
+            const [_0, _1, sender, _3, scopes, scripthash, method] = url.pathname.match(new RegExp('^//(([a-zA-Z0-9]+)(:(.*))?@)?([a-zA-Z0-9]+)/([a-zA-Z0-9]+)$'));
+            const ret = await neolineN3.invokeRead({
+                scriptHash: scripthash,
+                operation: method,
+                args: [...url.searchParams].map(([k, v]) => ({ type: k, value: v })),
+                signers: [{account: sender ?? await neolineN3.getAccount(), scopes: scopes?parseInt(scpoes):1}],
+            });
+            alert(JSON.stringify(ret));
+        } catch (err) {
+            alert(err);
         }
-        neolineN3.invokeRead(invoke).then(result => {
-            console.log('Read invocation result: ' + JSON.stringify(result));
-            window.alert(JSON.stringify(result));
-        }).catch((error) => {
-            const { type, description, data } = error;
-            switch (type) {
-                case 'NO_PROVIDER':
-                    window.alert('No provider available.');
-                    break;
-                case 'CONNECTION_REFUSED':
-                    window.alert('Connection dApp not connected. Please call the "connect" function.');
-                    break;
-                case 'RPC_ERROR':
-                    window.alert('There was an error when broadcasting this transaction to the network.');
-                    break;
-                default:
-                    // Not an expected error object.  Just write the error to the console.
-                    window.alert(error);
-                    break;
-            }
-        });
     });
 });
